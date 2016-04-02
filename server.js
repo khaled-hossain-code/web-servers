79067a0e2e141d89232eb56da7ecb813719928ce
Khaled Hossain <md.khaled.eee@gmail.com>
2016-04-02 16:59:47 +0600

    ready to deploy on heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  */
 var express = require('express');
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 var middleware = require('./middleware');
 
 app.use(middleware.logger); // this use has to be at the top for application level middleware and app.use means application level middleware
@@ -16,3 +16,4 @@ app.get('/admin',middleware.requireAuthentication, function(req, res){ // here s
 app.listen(PORT, function(){
   console.log('Express Server Started On: Localhost:'+ PORT);
 });
+
